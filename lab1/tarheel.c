3bf5cb068fb9c9ca05236331eeadb5fb3a3e5a6f
Audrey Sharp <Audrey@dhcp-v80-102023.wireless.unc.edu>
2016-09-30 13:57:06 -0400

    Add tarheel.c, working version of parts 1-6 and 9

---
+++ lab1/tarheel.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+char tarheel[] = 
+{ 
+"                                         :shhdhysso+:.                           \n"
+"	                          omyosdhooosyhmhyyyys/                       \n"
+"	                  .:+syhhdmyyys+++oshdmNNNNNNNm-                      \n"
+"	               .ohdhssydmh++++oydmmmNNmyyNNNNNd                       \n"
+"	             -ymhs+syhhmdosydmmNNNmdNy`-/dNNNN/     ``...``           \n"
+"	          -sddddhodhsmmdhhhdNmdhmm/`h: dmmmNNNs/oyhdddddddmdy+-       \n"
+"	         /md+/++/:hmhhddmmmh/h-`./s..s.-o+.dNmdhsooo+//oyhyhddmd+`    \n"
+"	         mm/+hmmh+/odNNm:/md..h..o:. .hyoosys+os+/+oss+//ss/sddmmd/   \n"
+"	        `md:hdsymmo:omNNy`/mh`:h`:mdssmdshhs+//oss/::///::/::+/ommmo  \n"
+"	         dm/dy++odmyh:sNms`/+`-mhhmdy+--:++oyh+::://:::oy/:y+::/++hm/ \n"
+"	         +mosdooosmh. `sNmhooyddy+-`   .../:`+do:::ss::ym+/dm+sd/:/dd`\n"
+"	        `-mm+dmdhmmdy- `+yys+:.`   ./yhhhy.   :ds/:hh:/oo++ooohdysoyN.\n"
+"	      -ohhhdd+dddms.od-          -shs/-.-+:    -dh+ooyhdmmmhhysoohdyN-\n"
+"	     ody+o+/ydyNNh-.`od-:- `  `:yh+.  `-`       -hmmmNNdmNy+++oyy+omN:\n"
+"	    /mo+ho++shoom.shy++oo`/o-+hh/``-/sh/    `    `:shmNh+md+++++dd+sNs\n"
+"	    /N++soho-` /h-h`:dd:  .sys/:/oss+:d:   `s-      `-/sydmddyyhdmh/md\n"
+"	    `yh///oossoyd+s+smNd    -shs:-shhym:    .yy/.`  ``  `.:oyyhs+hm:dm\n"
+"	     .ddo//yh+/+mshhhdh/    .hh ooymNmy` `.:+smmmhysyhyyso+- `/m+yh/my\n"
+"	.:+oyhdhdhss++shd/-....`     -h+yNmmdo`.oyyssssssydmNm+:--.-:+ysod+sm/\n"
+"	yNhyso/sosmdhy--+oyssyhy+     `:///-` :ds+/+ydyysyydmms:-:odhdhhdoomy \n"
+"	.+yyyyyyydmmdsoooos+:...`             ds:+syoshdhyo+sdmNmyo++omdosmy` \n"
+"	        `md......-:/+syso/.`          ds//::hhhmhmdys+odms+ohhs+hmo`  \n"
+"	         +do+-  -::-.` `:ohho-`       -hsds/sdmy++smms//hmdyo+yds-    \n"
+"	          .:dh-  `/dyo+-   -smy:   `   `/hmy++syyyydmmy//mmshh+.      \n"
+"	            .yms-` `   `  `  .ym`  y.     .:oosssssyymdo/hm+.         \n"
+"	              :ymho:.`    /o`  :   d+     `//::omNNNNNo//my           \n"
+"	                ./dNmdyyoooNd/`   `--yys+++yNNNmmmhhmh+/hd`           \n"
+"	                  .hdsohoodmsymysyhhhyymmooooo+:-` hmsyds`            \n"
+"	                    /hdysos+++ho+ohydmy/`          `::-`              \n"
+"	                      ./oyhdddddhyo+-`\n"
+};
+
+void goheels() {
+	printf("%s\n", tarheel);
+}
--- lab1/thsh.c
+++ lab1/thsh.c
@@ -1,22 +1,56 @@
-/* COMP 530: Tar Heel SHell */
+/* Tar Heel SHell 
+COMP 530 Lab 1
+
+Audrey Sharp (aud), PID 720473458
+Joshua Smith (joshuals), PID 720457161
+
+UNC Honor Pledge: I certify that no unauthorized assistance has been received or
+given in the completion of this work
+*/
 
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-#define MAX_INPUT 1024 // assume no input line will be longer than 1024 bytes
+#define MAX_INPUT 1024 //assume no input line will be longer than 1024 bytes
 
-int main (int argc, char **argv, char **envp) {
+int main (int argc, char **argv, char **envp) { //same as "int main (int argc, char *argv[], char *envp[])"
   setbuf(stdout, NULL); // disable buffering on stdout so printf works
-
+  
+  //char *testing = "hello";
+  //char testing[2][15];
+  //strcpy(testing[0], "hello");
+  //printf("%c\n", testing[0][1]);
+  //printf("%d\n", strlen(testing));
+  //printf("%s\n", testing+1);
+  
+  int debug = 0;
   int finished = 0;
   char cmd[MAX_INPUT];
   char *startprompt = "[";
   char *prompt = "] thsh> ";
-  char *cwd = (char *)malloc(MAX_INPUT);
+  char *cwd = (char *)malloc(MAX_INPUT); //or just "malloc(MAX_INPUT);"?
+  char tempStr[MAX_INPUT]; //for storing environment variable user input
+  char *vPath;
+  char tempVar[MAX_INPUT]; //for storing environment variable user input
+  char rv_char[2]; //store return value of child shell
+  
+  //add support for $? environment variable
+  setenv("?", "0", 1);
 
+  if (argv[1] != NULL) {
+    if (strcmp(argv[1], "-d") == 0) {
+      debug = 1;
+    }
+  }
+  
   while (!finished) {
+    memset(tempVar, 0, sizeof(tempVar));  //reset the char array to all null chars
+    memset(rv_char, 0, sizeof(rv_char));  //reset the char array to all null chars
     char *cursor;
     char last_char;
     int rv;
@@ -50,48 +84,202 @@ int main (int argc, char **argv, char **envp) {
 
     char *execute[MAX_INPUT];
     int i = 0;
+    char *input[MAX_INPUT];
+    char *output[MAX_INPUT];
+    char *pipes[MAX_INPUT];
+
+    // reset input/output/pipe vars
+    input[0] = NULL;
+    output[0] = NULL;
+    int k = 0;
+    /*for (k=0; k<j; k++) {
+      pipes[k] = NULL;
+    }*/
+
+    int j = 0; // counter for pipe
+    int in = 0; // boolean values for exec() later on
+    int out = 0;
+    int pipe = 0;
+    int changeOutput = 0;
+
     char *temp;
-    temp = strtok(cmd, " \n"); // split input by space and newline
+    temp = strtok(cmd, " =\n\t"); // split input by space, =, newline, and tab
     while (temp != NULL) {
-      execute[i] = temp;
-      temp = strtok(NULL, " \n");
-      i++;
+        if (strstr(temp, "<") != NULL || strstr(temp, ">") != NULL || strstr(temp, "|") != NULL) {
+          if (strstr(temp, "<") != NULL) {
+            in = 1;
+          if (temp[1] != '\0') { // no space between < and input file
+            input[0] = temp+1; // temp+1 to get rid of < at beginning of string
+          } else {
+            temp = strtok(NULL, " =\n\t");
+            continue;
+          }
+        } else if (strstr(temp, ">") != NULL) {
+          out = 1;
+          if (temp[0] != '>') { // number in front of >
+            if (temp[0] == '2') {
+              changeOutput = 1; // redirect to stderr
+            }
+            if (temp[2] != '\0') {
+              output[0] = temp+2;  
+            }
+          } else if (temp[1] != '\0') {
+            output[0] = temp+1;
+          } else {
+            temp = strtok(NULL, " =\n\t");
+            continue;
+          }
+        } else if (strstr(temp, "|") != NULL) {
+          pipe = 1;
+          if (temp[1] != '\0') {
+            pipes[j] = temp+1;
+            j++;
+          } else {
+            temp = strtok(NULL, " =\n\t");
+            continue;
+          }
+        }
+      }
+      if (in && input[0] == NULL) { // space between < and input file; put file in input[0]
+        input[0] = temp;
+      } else if (out && output[0] == NULL) {
+        output[0] = temp;
+      } else if (pipe && pipes[j] == NULL) {
+        pipes[j] = temp;
+        printf("input %sx ", pipes[j]);
+        j++;
+      } else if (in || out || pipe) {
+      } else {
+        //printf("putting in exec:%s\n", temp);
+        execute[i] = temp;
+        i++;
+      }
+
+      temp = strtok(NULL, " =\n\t"); // NULL used to get token from previous string
+      
     }
     execute[i] = NULL; // null terminate string
 
     // Execute the command, handling built-in commands separately 
     // write(1, cmd, strnlen(cmd, MAX_INPUT));
 
+    if (debug) {
+      fprintf(stderr, "RUNNING: %s\n", execute[0]);
+    }
+
     if (execute[0] == NULL) { // user entered a newline
       continue;
     } else if (strcmp(execute[0], "exit\n") == 0 || strcmp(execute[0], "exit") == 0) {
-      exit(0);
-    } else if ((strcmp(execute[0], "cd") == 0) && (execute[1] == NULL)) { // cd with no args
-      setenv("OLDPWD", getenv("PWD"), 1);
-      chdir(getenv("HOME"));
-      setenv("PWD", getenv("HOME"), 1);
-      continue;
-    } else if (strcmp(execute[0], "cd") == 0) {
-      setenv("OLDPWD", getenv("PWD"), 1);
-      chdir(execute[1]);
-      char tempcwd[MAX_INPUT];
-      setenv("PWD", getcwd(tempcwd, sizeof(tempcwd)), 1);
-      continue;
+        exit(0);
+    } else if (strcmp(execute[0], "goheels\n") == 0 || strcmp(execute[0], "goheels") == 0) {
+        goheels();
+        continue;
+    } else if (strcmp(execute[0], "cd") == 0) { 
+        if ((execute[1] == NULL) || (strcmp(execute[1], "~") == 0)) { // cd with no args
+            setenv("OLDPWD", getenv("PWD"), 1);
+            chdir(getenv("HOME"));
+            setenv("PWD", getenv("HOME"), 1);
+        } else if (strcmp(execute[1], "-") == 0) { //change to last directory user was in
+            strcpy(tempVar, getenv("PWD"));
+            chdir(getenv("OLDPWD"));
+            setenv("PWD", getenv("OLDPWD"), 1);
+            setenv("OLDPWD", tempVar, 1);
+        } else {
+            setenv("OLDPWD", getenv("PWD"), 1);
+            chdir(execute[1]);
+            char tempcwd[MAX_INPUT];
+            setenv("PWD", getcwd(tempcwd, sizeof(tempcwd)), 1);
+        }   
+        continue;   
+    } else if (strcmp(execute[0], "echo") == 0) {
+        // print empty line of input after "echo"
+        if (execute[1]==NULL) {
+            printf("\n");
+        } else if (execute[1]!=NULL) {    //print environment variable value
+            //if first character of input variable = $
+            if (execute[1][0] == '$') {
+                strncpy(tempVar, execute[1]+1, strlen(execute[1])-1);
+                vPath = getenv(tempVar); 
+                
+                //prints out the correct value if the variable exists
+                if (vPath != NULL) {
+                    printf("%s\n", vPath);
+                } else {        //print blank string if the variable doesn't exist
+                    printf("\n");
+                }
+                
+            } else {
+                fprintf(stderr, "ECHO error\n");
+            }       
+        }
+        continue;
+    } else if (strcmp(execute[0], "set") == 0) {
+        //print all environment variables if input is just "set"
+        if (execute[1]==NULL) {
+            char **env;
+            for (env = envp; *env!=0; env++) {
+                printf("%s\n", *env);
+            }
+        } else if (strcmp(execute[1], "*") == 0 || strcmp(execute[1], "@") == 0
+                   || strcmp(execute[1], "#") == 0 || strcmp(execute[1], "-") == 0
+                   || strcmp(execute[1], "$") == 0 || strcmp(execute[1], "!") == 0
+                   || strcmp(execute[1], "0") == 0 || strcmp(execute[1], "_") == 0) {
+            fprintf(stderr, "Error: invalid variable (*,@,#,-,$,!,0, and _ are not supported here)\n");
+        } else if ((execute[1]!=NULL) && (execute[2]!=NULL) && (execute[3]==NULL)) {
+            vPath = getenv(execute[1]); 
+            // assigns new value to existing variable or makes a new variable with a value
+            setenv(execute[1], execute[2], 1);
+        }
+        continue;
+    } else if (strcmp(execute[0], "jobs") == 0) {
+
+    } else if (strcmp(execute[0], "fg") == 0) { // make job x go to foreground
+
+    } else if (strcmp(execute[0], "bg") == 0) { // make job x go to background
+
     }
     
     pid_t pid = fork();
     if (pid == 0) {
+      if (in) {
+        int inputTemp = open(input[0], O_RDONLY);
+        dup2(inputTemp, STDIN_FILENO);
+        close(inputTemp);
+      }
+      if (out) {
+        int outputTemp = creat(output[0], 777);
+        if (changeOutput) { // output STDERR
+          dup2(outputTemp, STDERR_FILENO);
+        } else {
+          dup2(outputTemp, STDOUT_FILENO);
+        }
+        close(outputTemp);
+      }
+      /*if (pipe) {
+        pipe(pipes);
+        dup2(pipes[0], 0);
+        close(pipes[1]);
+      }*/
+
+      //printf("%s %s \n", execute[0], execute[1]);
       int temp = execvp(execute[0], execute);
       if (temp == -1) {
-        printf("Error - command not found\n");
+        fprintf(stderr, "Error - command not found\n");
       }
     } else if (pid < 0) {
-      printf("Error - fork failed\n");
+      fprintf(stderr, "Error - fork failed\n");
     } else {
       wait(&rv);
+      
+      //change the value of the int return variable to a char array in order to use setenv
+      sprintf(rv_char, "%d", WEXITSTATUS(rv));
+      setenv("?", rv_char, 1);
+    }
+
+    if (debug) {
+      fprintf(stderr, "ENDED: %s (ret=%d)\n", execute[0], WEXITSTATUS(rv));
     }
   }
 
   return 0;
 }
-
